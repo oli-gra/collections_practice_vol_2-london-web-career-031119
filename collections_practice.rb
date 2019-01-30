ae0aa98a6aef16420e7384f4d0f4d8575bc71da5
Oliver <oliver.grant@gmail.com>
2019-01-30 12:04:20 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -49,20 +49,6 @@ def find_cool (hash)#fimd cool hashes
   end
 end
 
-def reformat_languages(languages)
-  new_hash = {}
-  languages.each do |style, langs|
-    langs.each do |name, type|
-      binding.pry
-      if new_hash[name]
-        new_hash[name][:style] << style
-      else
-        new_hash[name] = {style: [style], type: type[:type] }
-      end
-    end 
-  end
-  new_hash
-end
 
 =begin
 (:schools) {
